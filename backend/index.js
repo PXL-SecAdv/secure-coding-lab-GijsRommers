28822a3fb15969c0f3c7b208d65574107a6046f7
Gijs Rommers <contactnanogijs@gmail.com>
2024-05-14 15:20:16 +0200

    password hashing and making sure all passwords are hashed

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors')
+const argon2 = require('argon2');
 
 const port=3000;
 
@@ -29,16 +30,40 @@ app.use(
 app.get('/authenticate/:username/:password', async (request, response) => {
     const username = request.params.username;
     const password = request.params.password;
+    const query = 'SELECT * FROM users WHERE user_name=$1';
+    pool.query(query, [username], async (error, results) => {
+        if (error) {
+            throw error
+        } else {
+            const storedPassword = results.rows[0]["password"]
+            try {
+                if (await argon2.verify(storedPassword, password)) {
+                    response.status(200).json({status: 'success'})
+                } else {
+                    response.status(401).json({status: 'Username or password is incorrect'})
+                }
+            } catch (err) {
+                response.status(500).json({status: 'Internal server error'})
+            }
+        }
+    })
+})
 
-    const query = `SELECT * FROM users WHERE user_name='${username}' and password='${password}'`;
-    console.log(query);
-    pool.query(query, (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)});
-      
-});
+
+
+async function hashAllPasswords() {
+    const query = "SELECT * FROM users WHERE password NOT LIKE '$argon2id$%'"
+    const users = await pool.query(query)
+    for (let user of users.rows) {
+        const password = user.password
+        const username = user['user_name']
+        const hashedPassword = await argon2.hash(password)
+        const updateQuery = 'UPDATE users SET password=$1 WHERE user_name=$2'
+        await pool.query(updateQuery, [hashedPassword, username])
+    }
+}
+
+setTimeout(hashAllPasswords, 5000);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
